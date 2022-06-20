08b3b8446e56b6fee2e10210daeaa7411d664b8f
annair99 <nushnair99@gmail.com>
2022-06-20 17:56:25 -0500

    added a table

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,9 @@
 document.getElementById("button").addEventListener("click", function(){
 	const palindrome = document.querySelector('#palindrome').value;
-	palindromeCheck(palindrome);
+	//call the function if string isnt empty
+	if (palindrome.length>0) {
+		addRow(palindromeCheck(palindrome)); 
+	}
 });
 
 function palindromeCheck(palindrome){
@@ -8,16 +11,31 @@ function palindromeCheck(palindrome){
     const palindromeClean = palindrome.replace(regex, "").toLowerCase();
 	const stringLength = palindromeClean.length;
 	const result = document.getElementById("result");
-	
-	let lastIndex = 1;
 
 	for (let counter = 0; counter < (stringLength/2) ; counter++) {
-		if (palindromeClean[counter] != palindromeClean[stringLength-lastIndex]) {
+		if (palindromeClean[counter] !== palindromeClean[stringLength-1-counter]) {
 			result.textContent = "Not a Palindrome!";
-			return;
-		} else {
-			lastIndex++;
-		}
-	result.textContent = "Palindrome!!!"; 
+			console.log(palindromeClean);
+			return result.textContent;
+		} 
 	}
+	result.textContent = "Palindrome!!!"; 
+	return result.textContent;
+}
+
+//currentResult is the output of palindromeCheck function, indicates whether a string is a palindrome or not
+function addRow(currentResult){
+	let n = 1;
+	var table = document.getElementById("resultsTable");
+	var NewRow = table.insertRow(n);
+
+	palindromeInput = document.getElementById("palindrome").value;
+	
+	var cel1 = NewRow.insertCell(0);
+	var cel2 = NewRow.insertCell(1);
+
+	cel1.innerHTML = palindromeInput;
+	cel2.innerHTML = currentResult;
+
+	n++;
 }
